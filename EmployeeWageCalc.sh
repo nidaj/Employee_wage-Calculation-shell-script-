828c7b4c2ba1b7d9285c21b919d8a6ab6355fe69
nida.j <nida.jawre@gmail.com>
2020-11-01 23:16:42 +0530

    Added:Attendance Check-(Presnt,Absent)

--- EmployeeWageCalc.sh
+++ EmployeeWageCalc.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
-echo "Welcome to Employee Wage Computation Program"
+#! /bin/bash
+echo "Welcome To Employee Wage Computation Program on Master Branch"
+attendance=$((RANDOM%2))
+if (( $attendance == 1 ))
+then
+        echo "Employee is Present"
+else
+        echo "Employee is Absent"
+fi
+
 
