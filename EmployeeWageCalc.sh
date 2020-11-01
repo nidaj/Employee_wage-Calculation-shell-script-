a12bd6cefadb8e856d24389f055fd6576588c5c8
nida.j <nida.jawre@gmail.com>
2020-11-02 00:09:41 +0530

    Added: Code from UC6 is Refactored using functions

--- EmployeeWageCalc.sh
+++ EmployeeWageCalc.sh
@@ -2,27 +2,46 @@
 echo "Welcome to Employee Wage Computation Program"
 
 sum=0
-full_wage=160
-half_wage=80
-no_wage=0
+#full_wage=160
+#half_wage=80
+#no_wage=0
+
+getWage()
+{
+         if (( $1 == 1 ))
+        then
+                wage=160
+        elif (( $1 == 2 ))
+        then
+                wage=80
+        else
+                wage=0
+        fi
+}
+getAttendance()
+{
+        attend=$((RANDOM%3))
+}
 
 echo "Day No        Wage        Total Wage"
 for (( d=1; d<=20; d++ ))
 do
-        attend=$((RANDOM%3))
+        getAttendance
         case "$attend" in
                 "1")
-                sum=$(( $sum + $full_wage ))
-                echo "Day $d         "$full_wage"        "$sum
+                getWage 1
+                sum=$(( $sum + $wage ))
+                echo "Day $d         "$wage"        "$sum
                 ;;
                 "2")
-                sum=$(( $sum + $half_wage ))
-                echo "Day $d         "$half_wage"         "$sum
+                getWage 2
+                sum=$(( $sum + $wage ))
+                echo "Day $d         "$wage"         "$sum
                 ;;
                 *)
-                sum=$(( $sum + $half_wage ))
-                echo "Day $d         "$no_wage"          "$sum
+                getWage 0
+                sum=$(( $sum + $wage ))
+                echo "Day $d         "$wage"          "$sum
                 ;;
         esac
 done
-
