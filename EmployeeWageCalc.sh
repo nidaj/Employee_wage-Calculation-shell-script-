ec6a9b0963b23074c6f77ef5eaf197ef0da01351
nida.j <nida.jawre@gmail.com>
2020-11-01 23:55:28 +0530

    Added: Part time employee wage calculation

--- EmployeeWageCalc.sh
+++ EmployeeWageCalc.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 echo "Welcome to Employee Wage Computation Program"
 
-attend=$((RANDOM%2))
+attend=$((RANDOM%3))
 full_wage=160
+half_wage=80
 no_wage=0
 
-if (( $attend == 0 ))
+if (( $attend == 1 ))
 then
         echo "Day No:" "Wage"
         echo "Day 1  "  $full_wage
+elif (( $attend == 2 ))
+then
+        echo "Day No:" "Wage"
+        echo "Day 1  "  $half_wage
 else
         echo "Day No:" "Wage"
         echo "Day 1  "  $no_wage
